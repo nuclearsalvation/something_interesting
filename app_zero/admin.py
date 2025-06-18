d5d2a01f0c6f485d0760ad806032ec8577294ff9
Anton Golubev <xiagenius@gmail.com>
2025-06-18 21:34:57 +0300

    admin

--- app_zero/admin.py
+++ app_zero/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import ZeroModel, ZeroImageModel, ZeroCSVModel
+from .models import ZeroModel, ZeroImageModel, ZeroCSVModel, ZeroNameNumModel, ZeroStringModel
 
 # Register your models here.
 class ZeroModelAdmin(admin.ModelAdmin):
@@ -8,3 +8,5 @@ class ZeroModelAdmin(admin.ModelAdmin):
 admin.site.register(ZeroModel)
 admin.site.register(ZeroImageModel)
 admin.site.register(ZeroCSVModel)
+admin.site.register(ZeroNameNumModel)
+admin.site.register(ZeroStringModel)
