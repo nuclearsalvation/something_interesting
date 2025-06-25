b2beedff9bcb0280b047f89db3d4d4f77eb432ca
Anton Golubev <xiagenius@gmail.com>
2025-06-25 21:54:48 +0300

    damping

--- app_zero/serializers.py
+++ app_zero/serializers.py
@@ -18,3 +18,13 @@ class ZeroNameNumSerializer(serializers.ModelSerializer):
     class Meta:
         model = ZeroNameNumModel
         fields = ['name', 'num']
+
+class ZeroDampingSerializer(serializers.Serializer):
+    a = serializers.FloatField()
+    b = serializers.FloatField()
+    c = serializers.FloatField()
+    dx = serializers.FloatField()
+    mu = serializers.FloatField()
+    fin = serializers.IntegerField()
+    class Meta:
+        fields = ['a', 'b', 'c', 'dx', 'fin', 'mu']
