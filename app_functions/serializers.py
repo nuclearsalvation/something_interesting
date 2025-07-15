f08203763628ab4d08b0d9d432cf0cde0ef504ba
Anton Golubev <xiagenius@gmail.com>
2025-07-15 21:05:22 +0300

    serializer

--- app_functions/serializers.py
+++ app_functions/serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+
+class FunctionsFourierSerializer(serializers.Serializer):
+    a0 = serializers.FloatField()
+    a1 = serializers.FloatField()
+    a2 = serializers.FloatField()
+    a3 = serializers.FloatField()
+    a4 = serializers.FloatField()
+    b1 = serializers.FloatField()
+    b2 = serializers.FloatField()
+    b3 = serializers.FloatField()
+    b4 = serializers.FloatField()
+    l = serializers.FloatField()
+    name = serializers.CharField()
+    description = serializers.CharField()
+    class Meta:
+        fields = ['a0', 'a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4', 'l', 'description']
